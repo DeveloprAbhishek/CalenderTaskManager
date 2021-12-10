c5c1c68cbff3e32cbfe43edba7e83619381bd244
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-12-10 21:14:57 +0530

    Initial commit

---
+++ app/src/main/java/com/masai/calendertaskmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.masai.calendertaskmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
